0fd4d482b16d03594d572051095631f3300be292
Lyux987 <lyux987@protonmail.com>
2021-06-20 23:34:05 -0400

    Agregando y aprendiendo variables, y comentario explicativo

--- js/01-holaMundo.js
+++ js/01-holaMundo.js
@@ -1,3 +1,3 @@
+// Comentario
 alert("Hola Mundo");
 alert("Bienvenido al Curso");
-document.write("Hola Mundo desde fichero externo")
---
+++ js/02-variables.js
@@ -0,0 +1,17 @@
+"use strict"
+/*Variables: Una variable es un contenedor de informacion o datos. Las variables deben tener un nombre. Si es un string(texto) debe ir dentro de las comillas ('' o "")
+Si es un numero entero sin ellas. Las variables se pueden reasignar, llamando a la variable y asignandole un nuevo valor NombreVar = "Nuevo valor", 
+Siempre y cuando: no sea una variable constante(const NombreVar = "Valor").
+
+Modo estricto ("use strict"): habilita funciones nuevas y por otra parte, evita errores o malas practicas, por ejemplo el crear variables sin la palabra "var" antes entre otros.
+*/
+
+var pais = "Venezuela";
+var continente = "Latino America";
+var antiguedad = 2021
+var paisContinente = pais + " " + continente
+
+pais = "España";
+continente = "Europa";
+console.log(pais, continente, antiguedad)
+alert(paisContinente)
